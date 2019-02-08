d5cc5889f339c8cde979b71a09b9f9f8e1964c0e
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-08 07:32:37 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
 
   #code your solution here:
     new_code = "dead"
-    epic_tragedy[:montague][:hero] = new_code
-    epic_tragedy[:capulet][:heroine] = new_code
+    epic_tragedy[:montague][:hero][:status] = new_code
+    epic_tragedy[:capulet][:heroine][:status] = new_code
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
